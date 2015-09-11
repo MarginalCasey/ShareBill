c7befeacb4ce0739720776c4be408273ee66fb97
Casey Chang <b00705008@ntu.edu.tw>
2015-09-12 00:11:59 +0800

    finish user

--- app/js/user.js
+++ app/js/user.js
@@ -8,10 +8,12 @@
 
     user.nextId = 0;
     user.list = [];
+
+    user.currIndex = '';
     user.currName = '';
     user.currColor = 'transparent';
 
-    //排序的參數設定
+    /*排序的參數設定*/
     user.sortableOptions = {
       axis: 'y',
       containment: "parent",
@@ -20,20 +22,42 @@
       revert: 200
     };
 
-    user.create = function() {
-      user.list.push({
-        id: user.nextId,
-        name: user.currName,
-        color: user.currColor
-      });
+    user.update = function() {
+      //新增user
+      if(user.currIndex === '') {
+        user.list.push({
+          id: user.nextId,
+          name: user.currName,
+          color: user.currColor
+        });
 
-      user.nextId++;
-    }
+        user.nextId++;
+      }
+      //修改user
+      else {
+        user.list[user.currIndex].name = user.currName;
+        user.list[user.currIndex].color = user.currColor;
+
+        user.currIndex = '';
+      }
+    };
+
+    user.delete = function() {
+      user.list.splice(user.currIndex, 1);
+      user.currIndex = '';
+    };
+
+    user.set = function(index) {
+      user.currIndex = index;
+      user.currName = user.list[index].name;
+      user.currColor = user.list[index].color;
+    };
 
     user.reset = function() {
+      user.currIndex = '';
       user.currName = '';
       user.currColor = 'transparent';
-    }
+    };
   });
 
   app.directive('userList', function(){
